8d819bbaf89e73da5c32579a9f1e5181b38446e7
Kaplan, L <contato.leonardokaplan@gmail.com>
2013-10-31 14:16:59 -0200

    walking in good paths

--- web.js
+++ web.js
@@ -1,13 +1,18 @@
 var express = require("express");
 var app = express();
 var cog_dir = '/nebula-fox';
+
 app.use(express.logger());
-process.env.PWD = process.cwd()
-app.use(express.static(process.env.PWD + cog_dir));
+
+process.env.PWD = process.cwd() + cog_dir;
+
+app.use(express.static(process.env.PWD));
+
 app.set('views', process.env.PWD);
 app.engine('html', require('ejs').renderFile);
+
 app.get('/', function(request, response) {
-  response.render(cog_dir + '/cog.html');
+  response.render(process.env.PWD + '/cog.html');
 });
 
 var port = process.env.PORT || 5000;
