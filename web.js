e4dc5e0785d78b3e961d32ce37ed9ebae938b4e7
Kaplan, L <contato.leonardokaplan@gmail.com>
2013-10-31 13:44:14 -0200

    generification

--- web.js
+++ web.js
@@ -1,12 +1,13 @@
 var express = require("express");
 var app = express();
+var cog_dir = '/nebula-fox';
 app.use(express.logger());
 process.env.PWD = process.cwd()
-app.use(express.static(process.env.PWD+'/nebula-fox'));
+app.use(express.static(process.env.PWD + cog_dir));
 app.set('views', process.env.PWD);
 app.engine('html', require('ejs').renderFile);
 app.get('/', function(request, response) {
-  response.render('nebula-fox/cog.html');
+  response.render(cog_dir + '/cog.html');
 });
 
 var port = process.env.PORT || 5000;
